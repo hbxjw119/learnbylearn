7e43d7b704453e0b49306fafa33cacd539363718
hbxjw119 <hbxjw2009@126.com>
2017-09-21 10:02:37 +0800

    modified readme and add decorator test

---
+++ decorator_test/test2.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from functools import wraps
+import random
+
+def outer_deco(size=10):
+	def func_cache(func):
+		cache = {}
+		@wraps(func)
+		def inner_deco(*args, **kwargs):
+			key = (args, frozenset(kwargs.items()))
+			if key not in cache:
+				print('func {} is not cached with arguments {} {}'.format(
+					func.__name__, args, kwargs)) 
+				res = func(*args, **kwargs)
+				if len(cache) >= size:
+					lucky_key = random.choice(list(cache.keys()))
+					print('func {} cache pop {}'.format(
+						func.__name__, lucky_key))
+					cache.pop(lucky_key, None)
+				cache[key] = res
+			return cache[key]
+		return inner_deco
+	return func_cache
+
+@outer_deco(size=3)
+def add_two_number(a, b):
+	return a + b
+
+@outer_deco()
+def product_two_number(a, b):
+	return a * b
+
+if __name__ == "__main__":
+	print('add_two_number func name is {}'.format(add_two_number.__name__))
+	print('1. add_two_number(1, 2)')
+	add_two_number(1, 2)
+	print('2. add_two_number(2, 3)')
+	add_two_number(2, 3)
+	print('3. add_two_number(1, b=2)')
+	add_two_number(1, b=2)
+	print('4. add_two_number(1, 2)')
+	add_two_number(1, 2)
+	print('5. product_two_number(1, 2)')
+	product_two_number(1, 2)
+	print('6. add_two_number(1, 3)')
+	add_two_number(1, 3)
--- readme.txt
+++
@@ -1,5 +0,0 @@
-Git is a distributed version control system.
-starting using github!
-2015/1/23
-
-本目录是学习python时的做的一些小脚本，供学习
